273410ee9ff4ec09a5c9626a0915fb1b8f369a22
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2019-04-21 19:19:52 +0200

    C output formatting++

--- src/main.rs
+++ src/main.rs
@@ -741,15 +741,15 @@ fn indent(out: &mut Write, nesting: usize) -> std::io::Result<()> {
 }
 
 fn generate_c_code<Int: BrainfuckInteger + Signed>(code: &Brainfuck<Int>, out: &mut Write) -> std::io::Result<()> {
-    write!(out, r##"
-#include <stdio.h>
+    write!(out, r##"#include <stdio.h>
 #include <sys/mman.h>
 
-int main() {{
-    {0}* mem = NULL; // TODO: mmap based memory handling with guard pages and singal handling
-    {0}* ptr = mem;
+{0}* mem = NULL; // TODO: mmap based memory handling with guard pages and singal handling
+{0}* ptr = mem;
 
+int main() {{
 "##, Int::c_type())?;
+
     let mut nesting = 1usize;
     for instr in code.iter() {
         match instr {
@@ -791,57 +791,76 @@ int main() {{
             },
 
             Instruct::WriteStr(data) => {
-                indent(out, nesting)?;
-                write!(out, "fwrite(\"")?;
+                if data.len() > 0 {
+                    indent(out, nesting)?;
+                    let multiline = if let Some(pos) = data.iter().position(|b| *b == b'\n') {
+                        pos < data.len() - 1
+                    } else {
+                        false
+                    };
 
-                for c in data.iter() {
-                    match *c {
-                        92u8 | 34u8 => {
-                            out.write_all(&[92u8, *c])?;
-                        },
+                    if multiline {
+                        write!(out, "fwrite(\n")?;
+                        indent(out, nesting + 1)?;
+                        write!(out, "\"")?;
+                    } else {
+                        write!(out, "fwrite(\"")?;
+                    }
 
-                        10u8 => {
-                            out.write_all(b"\\n")?;
-                        },
+                    for c in data.iter() {
+                        match *c {
+                            b'\\' | b'"' => {
+                                out.write_all(&[b'\\', *c])?;
+                            },
 
-                        0u8 => {
-                            out.write_all(b"\\0")?;
-                        },
+                            b'\n' => {
+                                if multiline {
+                                    out.write_all(b"\\n\"\n")?;
+                                    indent(out, nesting + 1)?;
+                                    write!(out, "\"")?;
+                                } else {
+                                    out.write_all(b"\\n")?;
+                                }
+                            },
 
-                        13u8 => {
-                            out.write_all(b"\\r")?;
-                        },
+                            b'\0' => {
+                                out.write_all(b"\\0")?;
+                            },
 
-                        9u8 => {
-                            out.write_all(b"\\t")?;
-                        },
+                            b'\r' => {
+                                out.write_all(b"\\r")?;
+                            },
 
-                        11u8 => {
-                            out.write_all(b"\\v")?;
-                        },
+                            b'\t' => {
+                                out.write_all(b"\\t")?;
+                            },
 
-                        8u8 => {
-                            out.write_all(b"\\b")?;
-                        },
+                            11u8 => {
+                                out.write_all(b"\\v")?;
+                            },
 
-                        c if c >= 32 && c <= 126 => {
-                            out.write_all(&[c])?;
-                        },
+                            8u8 => {
+                                out.write_all(b"\\b")?;
+                            },
 
-                        _ => {
-                            write!(out, "\\x{:02x}", c)?;
+                            c if c >= 32 && c <= 126 => {
+                                out.write_all(&[c])?;
+                            },
+
+                            _ => {
+                                write!(out, "\\x{:02x}", c)?;
+                            }
                         }
                     }
-                }
 
-                write!(out, "\", {}, 1, stdout);", data.len())?;
+                    write!(out, "\", {}, 1, stdout);", data.len())?;
+                }
             },
         }
     }
     write!(out, r##"
     return 0;
-}}
-    "##)?;
+}}"##)?;
     Ok(())
 }
 
